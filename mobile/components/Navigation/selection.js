2e3b0b07101cc30003ba0c5e131fe4d1473a948a
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2025-04-02 14:23:42 +0100

    add notifications

--- mobile/components/Navigation/selection.js
+++ mobile/components/Navigation/selection.js
@@ -1,6 +1,10 @@
 import { View, Text, StyleSheet, Button } from 'react-native';
-
+import  { usePushNotifications }  from '../notifications/expo-notif';
+import { StatusBar } from "expo-status-bar";
 const Selection = ({ navigation }) => {
+  const { expoPushToken, notification } = usePushNotifications();
+  const data = JSON.stringify(notification, undefined, 2);
+  console.log (`Token: ${expoPushToken?.data} ??`)
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>Select your connection type:</Text>
@@ -15,7 +19,13 @@ const Selection = ({ navigation }) => {
           title="WAN ACCESS"
           onPress={() => navigation.navigate('main')} // Navigate to WAN screen
         />
+        
       </View>
+      <View >
+      <Text>Token: {expoPushToken?.data ?? ""}</Text>
+      <Text>Notification: {data}</Text>
+      <StatusBar style="auto" />
+    </View>
     </View>
   );
 };
